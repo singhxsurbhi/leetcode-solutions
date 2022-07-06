875a716489fe1e371a0e37e4f6e8916b87dc2da8
Surbhi Singh <75158540+singhxsurbhi@users.noreply.github.com>
2022-07-06 14:38:03 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (49.33%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,6 +1,12 @@
 class Solution {
     public int fib(int n) {
         if(n<=1) return n;
-        else return fib(n-1)+fib(n-2);
+        int[] fibSeq=new int[31];
+        fibSeq[0]=0;
+        fibSeq[1]=1;
+        for(int i=2; i<=n;i++){
+            fibSeq[i]=fibSeq[i-1]+fibSeq[i-2];
+        }
+        return fibSeq[n];
     }
 }
