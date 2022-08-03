213abdacf1b68b1323de193ec1ffd212ef793598
Surbhi Singh <75158540+singhxsurbhi@users.noreply.github.com>
2022-08-03 22:39:10 +0530

    Time: 171 ms (12.17%), Space: 54.2 MB (71.10%) - LeetHub

---
+++ 729-my-calendar-i/729-my-calendar-i.java
@@ -0,0 +1,22 @@
+class MyCalendar {
+    List<int[]> calendar;
+
+    
+    public MyCalendar() {
+         calendar = new ArrayList();
+    }
+    
+    public boolean book(int start, int end) {
+         for (int[] slot: calendar) {
+            if (slot[0] < end && start < slot[1]) return false;
+        }
+        calendar.add(new int[]{start, end});
+        return true;
+    }
+}
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar obj = new MyCalendar();
+ * boolean param_1 = obj.book(start,end);
+ */
